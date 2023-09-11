b6d559c05924bee1cdc6682f589026a2105c9c5e
Anton Barabanov <anton.barabanov71995@gmail.com>
2023-09-11 20:26:57 +0300

    learned array

--- src/main/java/com/barabanovanton/Main.java
+++ src/main/java/com/barabanovanton/Main.java
@@ -1,7 +1,26 @@
 package com.barabanovanton;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        //Массивы
+        int[] intArr = new int[]{1, 421, 123};
+        int[] intArr0 = new int[3];
+        intArr0[0] = 1;
+        intArr0[1] = 421;
+        intArr0[2] = 123;
+        System.out.println("Длина массива " + intArr0.length);
+        System.out.println("Массив " + Arrays.toString(intArr0));
+
+        //Циклы
+        for(int i = 0; i < intArr0.length; i++){
+            System.out.println(intArr0[i]);
+        }
+
+
+
+
         //Целые числа
         byte varByte = 1; //-128 .. 127
         short varShort = 1; //-32768 .. 32767
@@ -41,9 +60,6 @@ public class Main {
         // 3. Сравнения >, <, >=, <=, !=, ==
 
         // 4. Логиские &&(и), ||(или), !(не)
-        if ((age >= 30) && (sex.equals("female"))){
-        }
-
         if (varStr != null && varStr.length() == 10){
             System.out.println("Строка состоит из 10 символов");
         } else if (varStr != null && varStr.length() == 13) {
@@ -52,5 +68,8 @@ public class Main {
         else {
             System.out.println("Строка состоит из другого количества символов");
         }
+
+
+
     }
 }
